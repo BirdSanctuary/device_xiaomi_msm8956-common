741f129d423883dfca824b13270e1f181cab2c84
Demon <demonsingur@gmail.com>
2017-09-20 09:08:07 +0000

    msm8956-common: Convert IR HAL into a native binderized HAL
    Change-Id: I8ddae1ba36d5a9cc16f0937189ff250eff3e7935
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -133,8 +133,7 @@ PRODUCT_PACKAGES += \
 
 # Consumerir
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    consumerir.msm8952
+    android.hardware.ir@1.0-service-custom
 
 # DataServices
 PRODUCT_PACKAGES += \
