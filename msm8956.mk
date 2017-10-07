2cff4922e2bc5e44271bc2458cecf2b1ef9eb171
Aashish Toshniwal <techybeing@gmail.com>
2017-10-07 22:29:37 +0530

    Update msm8956.mk

--- msm8956.mk
+++ msm8956.mk
@@ -22,7 +22,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/msm8956-common/msm8956-common-vendor.mk)
 
-PRODUCT_PROPERT_OVERRIDES += ro.cardinal.maintainer="Ashish T | Adarsh M.R"
+PRODUCT_PROPERT_OVERRIDES += ro.cardinal.maintainer="Aashish T | Adarsh M.R"
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
