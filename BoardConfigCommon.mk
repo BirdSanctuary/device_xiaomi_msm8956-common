155aa65073f045f72d885ba6405d9b4c3fce6979
Adarsh-MR <adarshmr1998@gmail.com>
2017-10-25 10:01:16 +0000

    msm8956-common: remove nostripping from dexpreopt
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -120,10 +120,6 @@ WITH_DEXPREOPT := true
 WITH_DEXPREOPT_DEBUG_INFO := false
 USE_DEX2OAT_DEBUG := false
 DONT_DEXPREOPT_PREBUILTS := true
-ifneq ($(TARGET_BUILD_VARIANT),user)
-# Retain classes.dex in APK's for non-user builds
-DEX_PREOPT_DEFAULT := nostripping
-endif
 endif
 endif
 endif
