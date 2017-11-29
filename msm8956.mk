7340ec2f82ec8a2e96c5663b737ce74a64dd8172
TheScarastic <warabhishek@gmail.com>
2017-11-29 15:35:47 +0530

    msm8956: Add custom biometrics to fix goodix fpsensor
     * Both fpc and goodix are supported
     * Do not use some notifying cancel hacks like https://github.com/AOSPA/android_frameworks_base/commit/acbb85687fc577ff74d0a1de93ccd63c2b05985a
       it is already implimented in Biometricservice and may cause issues.
     * SuperSu before SR5-superSU-v.2.82 seems to have an issue so please use latest SuperSu.
    
    co-authored with @alexstsv2
    
    Signed-off-by: TheScarastic <warabhishek@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiDoze
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.0-service-custom
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
