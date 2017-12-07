27b6a12d1983b19e34602ed18364115daea1a621
dianlujitao <dianlujitao@lineageos.org>
2017-12-07 15:15:00 +0100

    msm8956-common: Build a dummy android.hidl.base@1.0 for Oreo-MR0 blobs
    * needed for suzu perf stack
    
    Change-Id: I50d02364368691da2bc081988d500587cc427852

--- msm8956.mk
+++ msm8956.mk
@@ -201,6 +201,11 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0 \
     chargeonlymode
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0-java
+
 # HWUI
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.texture_cache_size=96 \
