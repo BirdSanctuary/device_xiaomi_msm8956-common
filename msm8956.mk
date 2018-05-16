f4fed367385da6b99f0af58cbf6d36770af8605f
Adarsh-MR <adarshmr1998@gmail.com>
2018-05-16 20:45:38 +0000

    msm8956-common: Back to passthrough Vibrator HAL
    * W/VibratorService(1197): Failed to perform effect (2), insufficient HAL version
    
    * E/VibratorService(1197): Failed to perform haptic effect: effect=2, strength=1, error=3749267571).
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -376,8 +376,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-impl
 
 # WiFi HAL
 PRODUCT_PACKAGES += \
