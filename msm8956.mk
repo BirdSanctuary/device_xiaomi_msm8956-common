f6a341ae66d83f88ec21a95e769351920ac492c3
Adarsh-MR <adarshmr1998@gmail.com>
2018-07-28 11:17:56 +0000

    Revert "msm8956-common: enforce more RRO overlay targets"
    This reverts commit 3a004011f6b2010fdc4c683828a0e841551a4208.

--- msm8956.mk
+++ msm8956.mk
@@ -20,12 +20,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res \
-    SettingsProvider \
-    SystemUI \
-    Bluetooth \
-    Settings
-    
+    framework-res
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/msm8956-common/msm8956-common-vendor.mk)
 
